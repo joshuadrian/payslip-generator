ef7b2efde0cb5569af36dd0a98e80b7772aa1ca1
Joshua Adrian <mrdoodlee@gmail.com>
2025-06-25 13:59:43 +0700

    feat: add GeneratesUid trait and uid column to users table

---
+++ app/Traits/GeneratesUid.php
@@ -0,0 +1,13 @@
+<?php
+
+use Illuminate\Support\Str;
+
+trait GeneratesUid
+{
+    protected static function bootGeneratesUid()
+    {
+        static::creating(function ($model) {
+            $model->uid = Str::random(7);
+        });
+    }
+}
