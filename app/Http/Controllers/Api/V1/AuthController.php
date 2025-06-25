a33de67ae0781b1c5e5bf9855e998b0b4a875987
Joshua Adrian <mrdoodlee@gmail.com>
2025-06-25 22:20:17 +0700

    refactor: update authentication method documentation

--- app/Http/Controllers/Api/V1/AuthController.php
+++ app/Http/Controllers/Api/V1/AuthController.php
@@ -6,23 +6,21 @@ use App\Exceptions\AuthException;
 use App\Traits\ApiResponse;
 use App\Http\Controllers\Controller;
 use App\Models\User;
+use Dedoc\Scramble\Attributes\Group;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Facades\Log;
 use Illuminate\Validation\ValidationException;
 
+#[Group('Authentication')]
 class AuthController extends Controller
 {
     use ApiResponse;
 
     /**
-     * Authenticate
-     *
-     * Get user's token for authentication
-     *
+     * Get authorization token
      * @unauthenticated
      */
-
     public function authenticate(Request $request)
     {
         try {
