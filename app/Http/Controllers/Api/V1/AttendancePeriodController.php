d8617935f959c59de4b948e3c19db2992f0f30d3
Joshua Adrian <mrdoodlee@gmail.com>
2025-06-25 23:27:28 +0700

    feat: implement index and store methods in AttendancePeriodController with success responses

--- app/Http/Controllers/Api/V1/AttendancePeriodController.php
+++ app/Http/Controllers/Api/V1/AttendancePeriodController.php
@@ -16,6 +16,25 @@ use Illuminate\Validation\ValidationException;
 class AttendancePeriodController extends Controller
 {
     use ApiResponse;
+    /**
+     *
+     * Display a listing of the resource.
+     */
+    public function index(Request $reqest)
+    {
+        $ap = AttendancePeriod::all();
+
+        /**
+         * Success
+         *
+         * @body array{
+         *      status:'success',
+         *      message: 'Successfully fetched attendance period data',
+         *      data: array<AttendancePeriod>
+         *  }
+         */
+        return $this->success('Successfully fetched attendance period data', $ap, 200);
+    }
 
     /**
      * Store a newly created resource in storage.
@@ -33,6 +52,15 @@ class AttendancePeriodController extends Controller
                 'end_date' => $request->end_date,
             ])->refresh();
 
+            /**
+             * Success
+             *
+             * @body array{
+             *      status:'success',
+             *      message: 'Successfully created new attendance period',
+             *      data: AttendancePeriod
+             *  }
+             */
             return $this->success(
                 'Successfully created new attendance period',
                 $ap->toResource(AttendancePeriodResource::class),
@@ -42,7 +70,35 @@ class AttendancePeriodController extends Controller
             return $this->error('Unprocessable Entity', $e->errors(), 422);
         } catch (\Throwable $th) {
             Log::error($th);
+
+            /**
+             * Internal server error
+             */
             return $this->error('Internal server error', [], 500);
         }
     }
+
+    /**
+     * Display the specified resource.
+     */
+    // public function show(AttendancePeriod $attendancePeriod)
+    // {
+    //     //
+    // }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    // public function update(Request $request, AttendancePeriod $attendancePeriod)
+    // {
+    //     //
+    // }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    // public function destroy(AttendancePeriod $attendancePeriod)
+    // {
+    //     //
+    // }
 }
