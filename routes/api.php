0bf14633b0f28c07365698120dea373350da4ba2
Joshua Adrian <mrdoodlee@gmail.com>
2025-06-25 15:55:03 +0700

    feat: add Laravel Sanctum for API authentication

---
+++ routes/api.php
@@ -0,0 +1,9 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+// Route::middleware('auth:sanctum')->group(function () {
+//     Route::name('v1')->group(function() {
+//         Route::apiResource('attendance-periods', );
+//     });
+// });
