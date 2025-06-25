33c83a4190557053f5a36b2bc5291d9613cfc3db
Joshua Adrian <mrdoodlee@gmail.com>
2025-06-25 15:23:51 +0700

    fix: update namespace for GeneratesUid trait in Salary and User models

--- app/Models/Salary.php
+++ app/Models/Salary.php
@@ -2,7 +2,7 @@
 
 namespace App\Models;
 
-use App\Traits\GeneratesUid;
+use App\Traits\Models\GeneratesUid;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
--- app/Models/User.php
+++ app/Models/User.php
@@ -4,7 +4,7 @@ namespace App\Models;
 
 // use Illuminate\Contracts\Auth\MustVerifyEmail;
 
-use App\Traits\GeneratesUid;
+use App\Traits\Models\GeneratesUid;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Relations\HasMany;
 use Illuminate\Foundation\Auth\User as Authenticatable;
