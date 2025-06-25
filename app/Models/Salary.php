c40b5fac4d0a540745e89b5d79869bf7c3085d3f
Joshua Adrian <mrdoodlee@gmail.com>
2025-06-25 14:12:50 +0700

    feat: create Salary model and migration for salaries table

---
+++ app/Models/Salary.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Models;
+
+use GeneratesUid;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Salary extends Model
+{
+    use HasFactory, GeneratesUid;
+
+    protected $fillable = ['salary'];
+
+    protected function casts(): array
+    {
+        return [
+            'salary' => 'decimal:2'
+        ];
+    }
+}
