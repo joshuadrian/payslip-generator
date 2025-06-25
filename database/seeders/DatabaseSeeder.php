2fb751ceaaac36dc60b9ea17ee44e874c26a02a9
Joshua Adrian <mrdoodlee@gmail.com>
2025-06-25 16:08:41 +0700

    feat: add RolePermissionSeeder for role and permission management

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -12,6 +12,8 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
+        $this->call([RolePermissionSeeder::class]);
+
         User::factory(100)
             ->hasSalaries(1)
             ->create();
---
+++ database/seeders/RolePermissionSeeder.php
@@ -0,0 +1,60 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use Spatie\Permission\Models\Permission;
+use Spatie\Permission\Models\Role;
+
+class RolePermissionSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        $permissions = [
+            ['name' => 'create attendance period'],
+
+            ['name' => 'create attendance'],
+
+            ['name' => 'create overtime'],
+
+            ['name' => 'create reimbursement'],
+
+            ['name' => 'create payroll'],
+
+            ['name' => 'create payslip'],
+
+            ['name' => 'create payslip summary'],
+        ];
+
+        $roles = [
+            [
+                'role' => ['name' => 'Admin'],
+                // 'permissions' => [
+                //     'create attendance period',
+                //     'create payroll',
+                //     'create payslip summary',
+                // ]
+            ],
+            [
+                'role' => ['name' => 'Employee'],
+                'permissions' => [
+                    'create attendance',
+                    'create overtime',
+                    'create reimbursement',
+                    'create payslip',
+                ]
+            ],
+        ];
+
+        array_walk($permissions, fn($d) => Permission::create($d));
+        array_walk($roles, function ($d) {
+            $role = Role::create($d['role']);
+            if (!empty($d['permissions'])) {
+                $role->syncPermissions($d['permissions']);
+            }
+        });
+    }
+}
