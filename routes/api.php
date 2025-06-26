80242d86b274d6368eac2429d78840aff2e6f009
Joshua Adrian <mrdoodlee@gmail.com>
2025-06-26 23:19:59 +0700

    refactor: comment out unused user index route in API

--- routes/api.php
+++ routes/api.php
@@ -9,7 +9,7 @@ use App\Http\Controllers\Api\V1\UserController;
 use Illuminate\Support\Facades\Route;
 
 Route::prefix('v1')->name('api.v1.')->group(function () {
-    Route::get('/users', [UserController::class, 'index']);
+    // Route::get('/users', [UserController::class, 'index']);
 
     Route::post('/auth', [AuthController::class, 'authenticate']);
 
